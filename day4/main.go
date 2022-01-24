0c43f35d47aea8f2869a32e81e89f122657362a5
David Schneider <dsbrng25b@gmail.com>
2022-01-24 01:16:07 +0100

    Add day4 part 1

---
+++ day4/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type board [5][5]uint8
+
+func (b *board) hasWinner(occured []uint8) bool {
+	return b.hasRowWinner(occured) || b.hasColWinner(occured)
+}
+
+func (b *board) hasColWinner(occured []uint8) bool {
+	for col := 0; col < 5; col++ {
+		colOk := true
+		for row := 0; row < 5; row++ {
+			rowOk := false
+			for _, n := range occured {
+				if n == b[row][col] {
+					rowOk = true
+					break
+				}
+			}
+			if !rowOk {
+				colOk = false
+				break
+			}
+		}
+		if colOk {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *board) hasRowWinner(occured []uint8) bool {
+	for _, row := range *b {
+		rowOk := true
+		for _, col := range row {
+			colOk := false
+			for _, n := range occured {
+				if n == col {
+					colOk = true
+					break
+				}
+			}
+			if !colOk {
+				rowOk = false
+				break
+			}
+		}
+		if rowOk {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *board) score(occured []uint8) int {
+	score := 0
+	for _, row := range *b {
+		for _, col := range row {
+			found := false
+			for _, num := range occured {
+				if col == num {
+					found = true
+					continue
+				}
+			}
+			if !found {
+				score += int(col)
+			}
+		}
+	}
+	return score
+}
+
+func findWinner(numbers []uint8, boards []board) []int {
+	indexes := []int{}
+	for i, board := range boards {
+		if board.hasWinner(numbers) {
+			indexes = append(indexes, i)
+		}
+	}
+	return indexes
+}
+
+func findWinnerAll(numbers []uint8, boards []board) (score int, winningNumber uint8, result int) {
+	for i := 1; i <= len(numbers); i++ {
+		winners := findWinner(numbers[:i], boards)
+		if len(winners) != 0 {
+			fmt.Println(boards[winners[0]])
+			score = boards[winners[0]].score(numbers[:i])
+			winningNumber = numbers[i-1]
+			result = score * int(winningNumber)
+			return
+		}
+	}
+	return 0, 0, 0
+}
+
+func read(input io.Reader) (boards []board, numbers []uint8, err error) {
+	scanner := bufio.NewScanner(input)
+	if !scanner.Scan() {
+		return nil, nil, fmt.Errorf("missing numbers")
+	}
+	numberStrings := strings.Split(scanner.Text(), ",")
+	numbers = make([]uint8, 0, len(numberStrings))
+	for _, s := range numberStrings {
+		number, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, nil, err
+		}
+		numbers = append(numbers, uint8(number))
+	}
+	boards = []board{}
+	currentBoard := board{}
+	rowIndex := 0
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			rowIndex = 0
+			currentBoard = board{}
+			continue
+		}
+
+		numbers := [5]uint8{}
+		numberStrings = strings.Fields(scanner.Text())
+		if len(numberStrings) != 5 {
+			return nil, nil, fmt.Errorf("invalid number of fields")
+		}
+		for i, str := range numberStrings {
+			number, err := strconv.Atoi(str)
+			if err != nil {
+				return nil, nil, err
+			}
+			numbers[i] = uint8(number)
+		}
+		currentBoard[rowIndex] = numbers
+		rowIndex++
+		if rowIndex == 5 {
+			boards = append(boards, currentBoard)
+		}
+	}
+	if scanner.Err() != nil {
+		return nil, nil, err
+	}
+	return boards, numbers, nil
+}
+
+func main() {
+
+	boards, numbers, err := read(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, board := range boards {
+		fmt.Println(board)
+	}
+	fmt.Println(len(boards))
+
+	score, number, result := findWinnerAll(numbers, boards)
+
+	fmt.Println("number", number)
+	fmt.Println("score", score)
+	fmt.Println("result", result)
+}
---
+++ day4/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+var exampleInput = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+14 21 17 24  4`
+
+func Test_part1(t *testing.T) {
+	b := bytes.NewBufferString(exampleInput)
+
+	boards, numbers, err := read(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(boards) != 3 {
+		t.Fatal("boards != 3")
+	}
+
+	noWinner := numbers[:11]
+	winners := findWinner(noWinner, boards)
+	if len(winners) != 0 {
+		t.Fatal("expected zero winners")
+	}
+
+	score, number, result := findWinnerAll(numbers, boards)
+
+	if number != 24 {
+		t.Fatalf("expected 24 as winning number. got=%d", number)
+	}
+	if score != 188 {
+		t.Fatalf("expected score to be 188. got=%d", score)
+	}
+
+	if result != 4512 {
+		t.Fatalf("expected result to be 4512. got=%d", result)
+	}
+
+}
